42d166dde57486b2a0bd8692f369bb8ecb5905bf
TheSov <arvid@redlinechicago.com>
2017-01-29 20:19:43 -0600

    Changes to be committed: 	modified:   index.html 	modified:   json/pgmap.php

--- json/pgmap.php
+++ json/pgmap.php
@@ -2,13 +2,12 @@
 
 header("Content-Type: application/json");
 
-$file = shell_exec('ceph status --format=json');
-$input_json = json_decode($file);
-$pgmap = preg_match('/v(\w*):.*/', $input_json->pgmap, $matches);
-
+#$file = shell_exec('ceph status --format=json');
+#print $file;
+#$input_json = json_decode($file);
+#$pgmap = preg_match('/(\w*):.*/', $input_json->pgmap, $matches);
+$matches[1]=100;
 $response = array('pgmap' => $matches[1]);
-
 $output = json_encode($response);
 print $output;
-
 ?>
