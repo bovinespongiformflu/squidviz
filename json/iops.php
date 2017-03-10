8d40e0fbd5ad76c1b45fc7a411757bfd377f99d4
TheSov <arvid@redlinechicago.com>
2017-03-10 01:43:54 -0600

    modified:   iops.php

--- json/iops.php
+++ json/iops.php
@@ -2,20 +2,18 @@
 
 header("Content-Type: application/json");
 
-$file = shell_exec('ceph pg stat');
+$file = shell_exec('ceph -s -f json');
+$input_json = json_decode($file, false, 100);
 
-$bits = explode(';',$file);
-$perf = $bits[2];
+$iopsr = $input_json->pgmap->read_op_per_sec;
+$iopsw = $input_json->pgmap->write_op_per_sec;
+$iops = $iopsr+$iopsw;
+$bytesr = $input_json->pgmap->read_bytes_sec;
+$bytesw = $input_json->pgmap->write_bytes_sec;
 
-#print $bits[2];
-$iops = 0;
-preg_match('@(\d+) op/s@i', $perf, $matches);
-$iops = $matches[1];
-
-$response = array('bytes_rd' => '', 'bytes_wr' => '', 'ops' => $iops);
+$response = array('bytes_rd' => $bytesr, 'bytes_wr' => $bytesw, 'ops' => $iops);
 
 $output = json_encode($response);
 
 print $output;
-
 ?>
